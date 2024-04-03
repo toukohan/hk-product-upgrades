90f67edda00da27089d7ff3a843f70ca0613ffe8
toukohan <toukoh@gmail.com>
2024-04-03 16:47:39 +0300

    comment out webpack entry customization

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require("path");
 
 module.exports = {
   ...defaultConfig,
-  entry: {
-    ...defaultConfig.entry,
-    main: path.resolve(process.cwd(), "src", "main.js"),
-    inputs: path.resolve(process.cwd(), "src", "inputs.js"),
-  },
+  // entry: {
+  //   ...defaultConfig.entry,
+  //   main: path.resolve(process.cwd(), "src", "main.js"),
+  //   inputs: path.resolve(process.cwd(), "src", "inputs.js"),
+  // },
 };
